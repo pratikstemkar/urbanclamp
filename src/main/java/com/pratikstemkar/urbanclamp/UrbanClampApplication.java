527a1a53b776cd61f4d1625868f276a4f49ed9ea
pratikstemkar <pratikstemkar@gmail.com>
2024-10-03 16:58:02 +0530

    Initial Commit :tada:

---
+++ src/main/java/com/pratikstemkar/urbanclamp/UrbanClampApplication.java
@@ -0,0 +1,13 @@
+package com.pratikstemkar.urbanclamp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrbanClampApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UrbanClampApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pratikstemkar/urbanclamp/UrbanClampApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pratikstemkar.urbanclamp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrbanClampApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
