a4ae478ac8ff8e3f8e5f22b74d0d23c84b197827
pratikstemkar <pratikstemkar@gmail.com>
2025-01-27 08:58:47 +0530

    validate token path in permit all

--- auth-service/src/main/java/xyz/urbanclamp/authservice/config/SecurityConfig.java
+++ auth-service/src/main/java/xyz/urbanclamp/authservice/config/SecurityConfig.java
@@ -31,7 +31,7 @@ public class SecurityConfig {
         http
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> {
-                    auth.requestMatchers("/api/auth/login/**", "/api/auth/register/**").permitAll();
+                    auth.requestMatchers("/api/auth/login/**", "/api/auth/register/**", "/api/auth/validate/**").permitAll();
                     auth.requestMatchers(HttpMethod.OPTIONS).permitAll();
                     auth.anyRequest().authenticated();
                 })
