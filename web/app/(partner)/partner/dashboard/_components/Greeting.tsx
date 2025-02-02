da03739cb5cdc44891c8913e760b0c752c686fc9
pratikstemkar <pratikstemkar@gmail.com>
2025-02-02 10:01:09 +0530

    deploy fix

--- web/app/(partner)/partner/dashboard/_components/Greeting.tsx
+++ web/app/(partner)/partner/dashboard/_components/Greeting.tsx
@@ -5,9 +5,7 @@ import { useAuth } from "@/store/hooks/useAuth";
 const Greeting = () => {
     const auth = useAuth();
     return (
-        <h1 className="font-bold text-2xl">
-            {"Hello, " + auth.user?.firstName + " " + auth.user?.lastName}
-        </h1>
+        <h1 className="font-bold text-2xl">{"Hello, " + auth.user?.name}</h1>
     );
 };
 
